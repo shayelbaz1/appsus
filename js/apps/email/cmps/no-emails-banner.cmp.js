1131125fc253f50a46e13ddfcd441b8c25410004
shayelbaz1 <shaysha6@gmail.com>
2020-06-27 11:27:51 +0300

    fix

--- js/apps/email/cmps/no-emails-banner.cmp.js
+++ js/apps/email/cmps/no-emails-banner.cmp.js
@@ -2,7 +2,7 @@ export default {
     props:['listType'],
     template: `
       <section class="no-email-banner flex align-center">
-          <h1>No {{listType}} Emails Left!</h2>
+          <h1>No {{listType}} Emails Left!</h1>
       </section>
       `,
     data() {
